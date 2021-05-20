8ea9ac20839119441c397f3abb10e133ae176af9
DavidKalen <48102023+DavidKalen@users.noreply.github.com>
2021-05-19 18:11:22 -0700

    Main File added, main engine of the program built.

---
+++ Main.py
@@ -0,0 +1,66 @@
+def value(x):
+    ret = 0
+
+    if x == 'א':
+        ret = 1
+    elif x == 'ב':
+        ret = 2
+    elif x == 'ג':
+        ret = 3
+    elif x == 'ד':
+        ret = 4
+    elif x == 'ה':
+        ret = 5
+    elif x == 'ו':
+        ret = 6
+    elif x == 'ז':
+        ret = 7
+    elif x == 'ח':
+        ret = 8
+    elif x == 'ט':
+        ret = 9
+    elif x == 'י':
+        ret = 10
+    elif x == 'כ' or x == 'ך':
+        ret = 20
+    elif x == 'ל':
+        ret = 30
+    elif x == 'מ' or x == 'ם':
+        ret = 40
+    elif x == 'נ' or x == 'ן':
+        ret = 50
+    elif x == 'ס':
+        ret = 60
+    elif x == 'ע':
+        ret = 70
+    elif x == 'פ' or x == 'ף':
+        ret = 80
+    elif x == 'צ' or x == 'ץ':
+        ret = 90
+    elif x == 'ק':
+        ret = 100
+    elif x == 'ר':
+        ret = 200
+    elif x == 'ש':
+        ret = 300
+    elif x == 'ת':
+        ret = 400
+
+    return ret
+
+
+with open('Chapter1.txt', encoding="utf8") as f:
+
+    f_contents = 0
+    temp = 0
+
+
+    while f_contents != '':
+        while f_contents != ' ':
+            temp += value(f_contents)
+            f_contents = f.read(1)
+            print(f_contents, end='')
+        f_contents = f.read(1)
+        print(f_contents, end='')
+        print(temp, end='')
+        temp = 0
